a0321384b5d482c1778e5f0e99bd5e05c0417753
Gustavo <gustavofreitas10@hotmail.com>
2021-08-30 12:03:02 -0300

    Finaliza API em SOLID com doc swagger de cadastro de categorias de carros

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,17 @@
 //  Importo a biblioteca de rotas do express.
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoriesController } from "../modules/cars/useCases/importCategories";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 //  Instacio as rotas.
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 //  Crio a rota
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -13,6 +19,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 //  Importo o express
 import express from "express";
+//  Importa o swagger, documentacao
+import swaggerUi from "swagger-ui-express";
 
+//  Importa as rotas
 import { router } from "./routes";
+//  Importa o arquivo Json com as informações da doc.
+import swaggerFile from "./swagger.json";
 //  Instancio o express.
 const app = express();
+app.use(express.json());
+//  Rota para documentacao
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
